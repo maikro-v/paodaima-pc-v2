1d2d8d562ff370a3575bfdf9273c58c3d35780fc
hsgzhuanyong <hsgzhuanyong@126.com>
2020-08-05 23:57:57 +0800

    style: 修改专栏样式

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -28,19 +28,18 @@
         </div>
       </div>
     </transition>
-    <header class="header">
-      <nav class="header__container nav">
-        <nuxt-link
-          v-for="item in menuNavList"
-          :key="item.id"
-          :to="`/article?type=${item.id}`"
-          class="nav__item"
-          :class="{ 'nav__item_active': _type === item.id }"
-        >
-          {{ item.name }}
-        </nuxt-link>
-      </nav>
-    </header>
+
+    <section class="tag-wrap container">
+      <nuxt-link
+        v-for="item in menuNavList"
+        :key="item.id"
+        :to="`/article?type=${item.id}`"
+        class="tag"
+        :class="{ 'tag_active': _type === item.id }"
+      >
+        {{ item.name }}
+      </nuxt-link>
+    </section>
     <div class="article-type">
       <main class="main">
         <el-row type="flex" :gutter="14">
@@ -236,7 +235,6 @@ export default {
 
 <style lang="scss" scoped>
   .article-type {
-    padding-top: 30px;
     background-color: #ffffff;
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'%3E%3Cdefs%3E%3ClinearGradient id='a' x1='0' x2='0' y1='1' y2='0' gradientTransform='rotate(121,0.5,0.5)'%3E%3Cstop offset='0' stop-color='%230FF'/%3E%3Cstop offset='1' stop-color='%23CF6'/%3E%3C/linearGradient%3E%3ClinearGradient id='b' x1='0' x2='0' y1='0' y2='1' gradientTransform='rotate(326,0.5,0.5)'%3E%3Cstop offset='0' stop-color='%23F00'/%3E%3Cstop offset='1' stop-color='%23FC0'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cg fill='%23FFF' fill-opacity='0' stroke-miterlimit='10'%3E%3Cg stroke='url(%23a)' stroke-width='10.23'%3E%3Cpath transform='translate(-15.4 -8.8) rotate(-8.8 1409 581) scale(0.934)' d='M1409 581 1450.35 511 1490 581z'/%3E%3Ccircle stroke-width='3.4100000000000006' transform='translate(-55 22) rotate(-4.4 800 450) scale(0.989)' cx='500' cy='100' r='40'/%3E%3Cpath transform='translate(-15.4 33) rotate(-55 401 736) scale(0.989)' d='M400.86 735.5h-83.73c0-23.12 18.74-41.87 41.87-41.87S400.86 712.38 400.86 735.5z'/%3E%3C/g%3E%3Cg stroke='url(%23b)' stroke-width='3.1'%3E%3Cpath transform='translate(132 13.2) rotate(-3.3 150 345) scale(1.022)' d='M149.8 345.2 118.4 389.8 149.8 434.4 181.2 389.8z'/%3E%3Crect stroke-width='6.820000000000001' transform='translate(33 -66) rotate(-79.2 1089 759)' x='1039' y='709' width='100' height='100'/%3E%3Cpath transform='translate(-8.8 -8.8) rotate(-13.2 1400 132) scale(0.89)' d='M1426.8 132.4 1405.7 168.8 1363.7 168.8 1342.7 132.4 1363.7 96 1405.7 96z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
     background-attachment: fixed;
@@ -268,42 +266,28 @@ export default {
       cursor: pointer;
     }
   }
-  .header {
-    width: 100%;
-    background: white;
-    border-bottom: 1px solid #f1f1f1;
-    @include container;
-    margin-top: 20px;
+
+  .tag-wrap {
+    margin: 40px auto;
+    font-size: 0;
   }
-  .nav {
-    &__item {
-      position: relative;
-      height: 40px;
-      line-height: 40px;
-      font-size: 16px;
-      color: #86858F;
-      padding: 0 20px;
-      display: inline-block;
-      &::before {
-        content: '';
-        position: absolute;
-        left: 50%;
-        bottom: 0;
-        width: 30px;
-        height: 2px;
-        background: transparent;
-        display: block;
-        transform: translate(-50%, 0);
-      }
-      &:hover,
-      &_active {
-        color: $primary;
-      }
-      &_active::before {
-        background: $primary;
-      }
+  .tag {
+    height: 28px;
+    line-height: 28px;
+    padding: 0 14px;
+    font-size: 14px;
+    color: #666;
+    display: inline-block;
+    margin: 0 15px 15px 0;
+    background: none;
+    border-radius: 14px;
+    cursor: pointer;
+    &_active {
+      background: $primary;
+      color: white;
     }
   }
+
   .main {
     width: 100%;
     @include container;
--- pages/special/index.vue
+++ pages/special/index.vue
@@ -1,11 +1,34 @@
 <template>
-  <layout hide-header>
+  <layout hide-header class="special">
     <header class="header">
-      <div class="container">
+      <div class="header__container container row align-center">
         <logo />
       </div>
     </header>
-    专栏
+    <section class="tag-wrap container">
+      <span class="tag">全部</span>
+      <span class="tag tag_active">全部</span>
+    </section>
+    <div class="container">
+      <el-row type="flex">
+        <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+          <div class="list row align-start">
+            <img src="@/assets/images/avatar.jpg" alt="" class="list__img">
+            <div class="list__detail col">
+              <h2 class="list__title text-over">
+                分布式链路追踪实战
+              </h2>
+              <p class="list__desc text-overs">
+                20讲玩转微服务可观测性系统
+              </p>
+              <p class="list__author">
+                作者
+              </p>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
   </layout>
 </template>
 
@@ -13,10 +36,85 @@
 import layout from '@/components/layout'
 import logo from '@/components/logo'
 export default {
-  components: { layout, logo }
+  components: { layout, logo },
+  head() {
+    return {
+      title: 'maikro - 专栏'
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .container {
+    max-width: 1200px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .special {
+    min-height: 100vh;
+    background: #f6f8ff;
+  }
+  .header {
+    height: 66px;
+    background: white;
+    box-shadow: 0 0 10px #ebedf4;
+    &__container {
+      height: 100%;
+    }
+  }
+  .tag-wrap {
+    margin: 40px auto;
+    font-size: 0;
+  }
+  .tag {
+    height: 28px;
+    line-height: 28px;
+    padding: 0 14px;
+    font-size: 14px;
+    color: #666;
+    display: inline-block;
+    margin: 0 15px 15px 0;
+    background: none;
+    border-radius: 14px;
+    cursor: pointer;
+    &_active {
+      background: $primary;
+      color: white;
+    }
+  }
+  .list {
+    padding: 20px;
+    height: 172px;
+    background: #fff;
+    border-radius: 8px;
+    margin-bottom: 20px;
+    box-sizing: border-box;
+    position: relative;
+    &__img {
+      width: 100px;
+      height: 130px;
+      border-radius: 8px;
+    }
+    &__detail {
+      width: 0;
+      margin-left: 10px;
+      margin-top: 4px;
+    }
+    &__title {
+      font-size: 20px;
+      font-weight: 500;
+    }
+    &__desc {
+      font-size: 14px;
+      margin-top: 6px;
+    }
+    &__author {
+      position: absolute;
+      bottom: 32px;
+      left: 130px;
+      font-size: 12px;
+      color: #999;
+    }
+  }
 </style>
