f0cc707eed754c0eba6ae65c77eeba70d1dfdd24
hsgzhuanyong <hsgzhuanyong@126.com>
2020-08-31 23:04:32 +0800

    feat: 增加上传文件脚本

---
+++ scripts/upload.js
@@ -0,0 +1,26 @@
+const Ftp = require('./Ftp')
+
+const HOST = '39.107.86.114'
+const USERNAME = 'root'
+const PASSWORD = 'Zhuanyong1'
+const PORT = 22
+
+const ftp = new Ftp()
+
+ftp.connect({
+  host: HOST,
+  username: USERNAME,
+  password: PASSWORD,
+  port: PORT
+}).then(async(conn) => {
+  const remoteRoot = '/home/www/paodaima/views/pc/'
+  await ftp.removeFile(remoteRoot, ['.nuxt', 'server', 'static', 'nuxt.config.js', 'package.json'])
+  await ftp.uploadDir('.nuxt', remoteRoot + '.nuxt')
+  await ftp.uploadDir('server', remoteRoot + 'server')
+  await ftp.uploadDir('static', remoteRoot + 'static')
+  await ftp.uploadFile('nuxt.config.js', remoteRoot + 'nuxt.config.js')
+  await ftp.uploadFile('package.json', remoteRoot + 'package.json')
+  ftp.end()
+}).catch((err) => {
+  console.log(err)
+})
